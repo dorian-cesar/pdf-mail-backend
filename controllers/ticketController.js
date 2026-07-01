b6966137ff5fcb5dc31eef938eaa292722e7e967
Diegowigo <dwigodski@gmail.com>
2026-07-01 10:35:07 -0400

    add: se agrega parámetro de opción en controlador de ticket para generar pdf

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -31,7 +31,10 @@ exports.generateTicket = async (req, res) => {
 
     const logoFile = allowedLogos.includes(logo) ? logo : "logo-boletos.png";
     const logoPath = path.join(__dirname, "../public/images", logoFile);
-    const partnerLogoPath = path.join(__dirname, "../public/images/logo-boletos.png");
+    const partnerLogoPath = path.join(
+      __dirname,
+      "../public/images/logo-boletos.png",
+    );
 
     let logoBase64 = "";
     let logoPartnerBase64 = "";
@@ -47,7 +50,6 @@ exports.generateTicket = async (req, res) => {
       console.error("Error al leer los logos:", err.message);
     }
 
-    // 🔥 AQUÍ ESTÁ LA MEJORA (soporta async o sync automáticamente)
     const html = await Promise.resolve(
       selectedTemplate({ ...data, logoBase64, logoPartnerBase64 }),
     );
@@ -56,7 +58,11 @@ exports.generateTicket = async (req, res) => {
     let options = {
       format: "A4",
       printBackground: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+      ],
     };
 
     let file = { content: html };
